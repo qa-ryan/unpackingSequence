a39cc7627d579331ca52851229e1c59cf9cf6529
Ryan Hermawan <ryan.hermawan@gameloft.com>
2023-03-30 08:00:28 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+if __name__ == 'mai'
